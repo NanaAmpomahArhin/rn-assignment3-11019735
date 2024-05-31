3f082a37f620dab94af978aa23f3069ea0044cab
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-05-31 14:44:05 -0700

    added data for the ongoing tasks

---
+++ assignment3/Data/ongoingTasksData.js
@@ -0,0 +1,62 @@
+export const ongoingTaskData = [
+  {
+    id: 0,
+    name: "Mobile App Development",
+  },
+  {
+    id: 1,
+    name: "Web Development",
+  },
+  {
+    id: 2,
+    name: "Push Ups",
+  },
+  {
+    id: 3,
+    name: "Sit Ups",
+  },
+  {
+    id: 4,
+    name: "Watching Soccer",
+  },
+  {
+    id: 5,
+    name: "Playing a PlayStation",
+  },
+  {
+    id: 6,
+    name: "Eating Rice and Stew ",
+  },
+  {
+    id: 7,
+    name: "Cooking Jollof",
+  },
+  {
+    id: 8,
+    name: "Ballet",
+  },
+  {
+    id: 9,
+    name: "Salsa",
+  },
+  {
+    id: 10,
+    name: "Break Dancing",
+  },
+  {
+    id: 11,
+    name: "WhatsApping",
+  },
+  {
+    id: 12,
+    name: "Calcus II",
+  },
+  {
+    id: 13,
+    name: "Karaoke",
+  },
+  {
+    id: 14,
+    name: "Beat Boxing",
+  },
+];
