f49a3999d5a12bf68fe18205401de637d60bf790
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-05-31 12:44:52 -0700

    created the top part of the app which involves the welcome text, the taks to do and the person icon

--- assignment3/App.js
+++ assignment3/App.js
@@ -1,9 +1,36 @@
-import { SafeAreaView } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView
-      style={{ backgroundColor: "#F7F0E8", flex: 1 }}
-    ></SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "#F7F0E8", flex: 1 }}>
+      <View style={{ padding: 15, gap: 20 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+              Hello, Devs
+            </Text>
+            <Text style={{ fontWeight: "normal" }}>14 tasks today</Text>
+          </View>
+
+          <TouchableOpacity
+            style={{ backgroundColor: "white", borderRadius: 999, padding: 8 }}
+          >
+            <Image source={require("./assets/person.png")} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
