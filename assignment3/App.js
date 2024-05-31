3c86ee42f7e83b583cb277543dbcc698ce43f46f
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-05-31 16:48:21 -0700

    removed all the in-line styles and used the styles file to style the components

--- assignment3/App.js
+++ assignment3/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Button,
   FlatList,
@@ -10,81 +11,41 @@ import {
 } from "react-native";
 import { categoriesData } from "./Data/categoriesData";
 import { ongoingTaskData } from "./Data/ongoingTasksData";
+import styles from "./styles";
 
 export default function App() {
   return (
-    <ScrollView style={{ backgroundColor: "#F7F0E8", flex: 1 }}>
-      <View style={{ padding: 15, gap: 20 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
+    <ScrollView style={styles.container}>
+      <View style={styles.wrapper}>
+        <View style={styles.headingContainer}>
           <View>
-            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
-              Hello, Devs
-            </Text>
-            <Text style={{ fontWeight: "normal" }}>14 tasks today</Text>
+            <Text style={styles.headingText}>Hello, Devs</Text>
+            <Text style={styles.subheadingText}>14 tasks today</Text>
           </View>
-
-          <TouchableOpacity
-            style={{ backgroundColor: "white", borderRadius: 999, padding: 8 }}
-          >
+          <TouchableOpacity style={styles.buttonContainer}>
             <Image source={require("./assets/person.png")} />
           </TouchableOpacity>
         </View>
 
-        {/**The Search Bar */}
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          {/**The TextInput */}
-          <TextInput
-            placeholder="Search"
-            style={{
-              backgroundColor: "white",
-              width: "80%",
-              height: 50,
-              borderRadius: 10,
-              padding: 5,
-            }}
-          />
-          {/**The Filter Icon */}
-          <View
-            style={{ backgroundColor: "#F0522F", padding: 8, borderRadius: 10 }}
-          >
+        {/* The Search Bar */}
+        <View style={styles.searchBarContainer}>
+          {/* The TextInput */}
+          <TextInput placeholder="Search" style={styles.textInput} />
+          {/* The Filter Icon */}
+          <View style={styles.filterIconContainer}>
             <Image source={require("./assets/Vector.png")} />
           </View>
         </View>
 
-        {/**Categories */}
-        <View style={{ gap: 10 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 20, letterSpacing: 1 }}>
-            Categories
-          </Text>
-
+        {/* Categories */}
+        <View style={styles.categoriesContainer}>
+          <Text style={styles.categoriesHeading}>Categories</Text>
           <FlatList
             data={categoriesData}
             renderItem={({ item }) => (
-              <View
-                style={{
-                  gap: 10,
-                  backgroundColor: "white",
-                  marginRight: 5,
-                  padding: 15,
-                  borderRadius: 10,
-                }}
-              >
+              <View style={styles.categoryCard}>
                 <View>
-                  <Text
-                    style={{
-                      fontWeight: "bold",
-                      fontSize: 20,
-                      letterSpacing: 1,
-                    }}
-                  >
-                    {item.name}
-                  </Text>
+                  <Text style={styles.categoryCardHeading}>{item.name}</Text>
                   <Text>{item.description}</Text>
                 </View>
                 <Image source={item.image} />
@@ -96,50 +57,25 @@ export default function App() {
           />
         </View>
 
-        {/**Ongoing Tasks */}
-        <View style={{ gap: 20 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 20, letterSpacing: 1 }}>
-            Ongoing Task
-          </Text>
-
+        {/* Ongoing Tasks */}
+        <View style={styles.ongoingTasksContainer}>
+          <Text style={styles.ongoingTasksHeading}>Ongoing Task</Text>
           <FlatList
             data={ongoingTaskData}
             renderItem={({ item }) => (
-              <View
-                style={{
-                  gap: 10,
-                  height: 150,
-                  backgroundColor: "white",
-                  borderWidth: 1,
-                  borderColor: "#E8D1BA",
-                  marginRight: 5,
-                  padding: 15,
-                  borderRadius: 10,
-                  width: "100%",
-                  marginBottom: 5,
-                  justifyContent: "center",
-                }}
-              >
+              <View style={styles.ongoingTaskCard}>
                 <View>
-                  <Text
-                    style={{
-                      fontWeight: "medium",
-                      fontSize: 20,
-                      letterSpacing: 1,
-                    }}
-                  >
-                    {item.name}
-                  </Text>
+                  <Text style={styles.ongoingTaskName}>{item.name}</Text>
                 </View>
               </View>
             )}
             keyExtractor={(item) => item.id}
           />
+        </View>
 
-          {/**Button */}
-          <View>
-            <Button title="Click Me" onPress={() => alert("Button Clicked")} />
-          </View>
+        {/* Button */}
+        <View>
+          <Button title="Click Me" onPress={() => alert("Button Clicked")} />
         </View>
       </View>
     </ScrollView>
