e7d869a0e76070643ecd0843863d67822152d4a4
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-05-31 14:02:20 -0700

    added data for the categories

---
+++ assignment3/Data/categoriesData.js
@@ -0,0 +1,50 @@
+export const categoriesData = [
+  {
+    id: 0,
+    name: "Exercise",
+    description: "12 Task",
+    image: require("../assets/woman1.png"),
+  },
+  {
+    id: 1,
+    name: "Study",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 2,
+    name: "Cook",
+    description: "12 Task",
+    image: require("../assets/woman1.png"),
+  },
+  {
+    id: 3,
+    name: "Sleep",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 4,
+    name: "Eat",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 5,
+    name: "Play",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 6,
+    name: "Chat",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+  {
+    id: 7,
+    name: "Dance",
+    description: "12 Task",
+    image: require("../assets/woman.png"),
+  },
+];
